23de8d0e7adc3554de95f0a8266b1a6601456174
thayaneibduarte <thayduarte04@gmail.com>
2025-03-23 19:57:44 -0300

    Adicionando comentários e realizando a impressão dos Valores

--- Cating-e-Locale/src/Main.java
+++ Cating-e-Locale/src/Main.java
@@ -2,10 +2,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        double pi = 3.14159;
+        //Declarando a Variável Pi
+        double pi = 3.14159141591415914159;
+
+        //Conversão de Pi para Float
         float b = (float) pi;
 
+        //Conversão de Float para Int
         int c = (int) b;
 
+        //Imprimindo os resultados
+        System.out.println(pi);
+        System.out.println(b);
+        System.out.println(c);
     }
 }
